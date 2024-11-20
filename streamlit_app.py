89f417f01cf4c1010a789e9e921025dc5812db08
altonFreitas <162043681+altonFreitas@users.noreply.github.com>
2024-11-20 12:10:38 +0000

    importar as bibliotecas necessarias e configurar código para visualizaçao de gráfico

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
-
+import pandas as pd
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "How do i started the dataset in my rep"
 )
+df = pd.read_csv('partos-e-cesarianas.csv', sep=';', on_bad_lines='skip')
+print(df.head())  # Analise os dados para inconsistências
+df.columns = df.columns.str.strip()
+
+# Verificar e limpar dados inválidos
+df = df.dropna(subset=['Nº Total de Partos'])  # Remove linhas com valores NaN
+
+# Exibir o gráfico
+st.line_chart(df['Nº Total de Partos'])
+
